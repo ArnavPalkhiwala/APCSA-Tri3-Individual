5d9414fe09c0df341591e7758e22343a86c58be0
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2022-03-14 05:35:13 +0000

    first challenge blueprint finished -- just have to add more classes as needed which is a repetitive process

---
+++ InitialTest.java
@@ -0,0 +1,10 @@
+public class InitialTest extends abstractClass {
+    public InitialTest(String text) {
+        super(text);
+    }
+
+    public void run() {
+      //more to be added later
+        System.out.println("Initial test");
+    }
+}
