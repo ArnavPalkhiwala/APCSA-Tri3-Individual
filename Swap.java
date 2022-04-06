07225ddf766a571ff5e049a77131046309907d0b
wiz124 <hli16492@gmail.com>
2022-04-06 10:17:36 -0700

    simplified swap function and queue menu in challenge1

--- Swap.java
+++ Swap.java
@@ -1,7 +1,13 @@
+//something broke here
+//don't know what but I changed ur code a bit so that it was less complicated than it needed to be
+//it would break if i entered the same integer twice
+
 public class Swap{
   void run(int num1, int num2){
-    int[] r = {num1, num2};
-    System.out.println("Before: " + (r[0]) + " " + (r[1]));
+  //  int[] r = {num1, num2};
+
+    System.out.println("Before: " + num1 + " " + num2);
+/*
     for(int i = 0; i < r.length; i++){
       if(r[i] < r[i+1]){
         int x = r[i];
@@ -9,8 +15,14 @@ public class Swap{
         r[i+1] = x;
         break;
       }
-    }
-    System.out.println("After: " + (r[0]) + " " + (r[1]));
+    }*/
+
+    //simpler swap
+    int temp= num1;
+    num1= num2;
+    num2=temp;
+
+    System.out.println("After: " + num1 + " " + num2);
     
   }
 }
